faadd97f4c26f4683aefe892070ed0999fe30910
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:05:08 +0530

    Hemanth : Find Power of Two until N

---
+++ Powerof2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Powerof2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, power = 1;
+            Console.WriteLine("Enter Number Between 2 to 30");
+            int num = int.Parse(Console.ReadLine());
+
+            while(i<=num)
+            {
+                Console.WriteLine(i + " " + power);
+                power = 2 * power;
+                i++;
+            }
+
+
+        }
+    }
+}
