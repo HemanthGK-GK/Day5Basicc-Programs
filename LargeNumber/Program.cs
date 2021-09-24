d6e980fbd16f8afc955d9d38a322876d8580fc4e
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:01:58 +0530

    Hemanth : Find Larget of Three number

---
+++ LargeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LargeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number :");
+           int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second number :");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Third number :");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if(num1>num2 && num1>num3)
+                Console.Write("The Greater Number is : "+num1);
+            else if (num2 > num1 && num2 > num3)
+                Console.Write("The Greater Number is : " + num2);
+            else
+                Console.Write("The Greater Number is : " + num3);
+        }
+    }
+}
