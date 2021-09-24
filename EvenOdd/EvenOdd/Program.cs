a371f224a256e205cbf5876eb0a5f11b0e2badc3
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 07:56:39 +0530

    Hemanth : Find Ther number is Even or Odd

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+                Console.WriteLine("Number is Even");
+            else
+                Console.WriteLine("Number is Odd");
+        }
+
+    }
+}
