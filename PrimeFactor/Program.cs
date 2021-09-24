64398811ca22f01fcb561da975464b7cc3a71678
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:06:40 +0530

    Hemanth : Find the Prime Factor of the given Number

---
+++ PrimeFactor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class PrimeFactor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int n = int.Parse(Console.ReadLine());
+
+            primeFactor(n);
+             
+        }
+        public static void primeFactor(int n)
+        {
+            while(n%2==0)
+            {
+                Console.Write(2 + " ");
+                n /= 2;
+            }
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+                }
+            }
+            if (n > 2)
+                Console.Write(n);
+
+        }
+    }
+}
