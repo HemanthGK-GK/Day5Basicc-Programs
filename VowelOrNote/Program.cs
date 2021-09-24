8b6b5b23acde3dff91c2a83db7a8358593a09f59
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:11:52 +0530

    Hemanth : Find Given Alphabet is Vowel Or Consonent

---
+++ VowelOrNote/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VowelOrNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an Alphabet to check Vowel or Consonant ");
+            char alpha = Convert.ToChar(Console.ReadLine());
+
+            if (alpha ==  'a'|| alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u' ||
+                alpha == 'A' || alpha == 'E' ||alpha == 'I' || alpha == 'O' || alpha == 'U' ||)
+            {
+                Console.WriteLine(alpha + " Is a Vowel");
+            }
+            else
+                Console.WriteLine(alpha + " Is Consonent");
+        }
+    }
+}
