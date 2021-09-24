e231c6fd67f2c61b61d8720dbf82329c8f3addfd
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:09:58 +0530

    Hemanth : Swap two Numbers

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number1");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number2");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before swapping");
+            Console.WriteLine("Number1 : "+num1);
+            Console.WriteLine("Number2 : " + num2);
+
+            int temp = 0;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("After swapping");
+            Console.WriteLine("Number1 : " + num1);
+            Console.WriteLine("Number2 : " + num2);
+
+        }
+    }
+}
