bf5d8d2a4d9f2cf01f1456ca0163d4630288d782
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:03:29 +0530

    Hemanth : Find given Year is Leap Year or Not

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year to Check");
+            int year = int.Parse(Console.ReadLine());
+
+            if(( year%4 == 0 && year%100 != 0) || year%400==0)
+                Console.WriteLine(year + " is leap year ");
+            else
+                Console.WriteLine(year + " is not a leap year ");
+        }
+    }
+}
