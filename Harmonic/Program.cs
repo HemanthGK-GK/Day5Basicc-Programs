2b4dfb1a20d14c4a6aac3157880730b38a939b5d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:00:34 +0530

    Hemanth : Print Harmonic Series

---
+++ Harmonic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harmonic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int Num = int.Parse(Console.ReadLine());
+            Console.Write(1);
+            for(int i=1;i<=Num;i++)
+            {
+                Console.Write(" + 1/"+i);
+            }
+        }
+    }
+}
