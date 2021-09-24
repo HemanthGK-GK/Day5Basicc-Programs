1d8b9e6f14edfd8d6279dc273f1d766b69512386
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-24 08:08:41 +0530

    Hemanth : Find Remainder and Quotient

---
+++ RemainderandQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RemainderandQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Devider ");
+            int div = int.Parse(Console.ReadLine());
+
+             Console.WriteLine("Remainder : "+ num % div);
+            Console.WriteLine("Quotient : " + num / div);
+
+
+        }
+    }
+}
